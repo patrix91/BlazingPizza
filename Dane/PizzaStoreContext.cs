475f7caa0551dbb09e56475f2c2a25017e6d6de3
Patryk Wiśniewski <patrix91@users.noreply.github.com>
2024-08-06 17:49:34 +0200

    dodanie SpecialController.cs

---
+++ Dane/PizzaStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazingPizza.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BlazingPizza.Dane;
+
+public class PizzaStoreContext : DbContext
+{
+    public PizzaStoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<PizzaSpecial> Specials { get; set; }
+}
