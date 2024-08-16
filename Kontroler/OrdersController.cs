e434457eec4f78ae09b8cf6b665a7650730d1556
Patryk Wiśniewski <patrix91@users.noreply.github.com>
2024-08-16 14:14:35 +0200

    Dodano obsługę zamówień i poprawiono nawigację
    - Dodano przestrzeń nazw `BlazingPizza.Model` i `BlazingPizza.Dane` w pliku `OrdersController.cs`.
    - Zmieniono atrybuty `[Route]` i `[ApiController]` w `OrdersController.cs`, przenosząc je bezpośrednio nad deklarację klasy.
    - Przeniesiono inicjalizację `PizzaStoreContext _db` do konstruktora w `OrdersController.cs`.
    - Dodano metodę `GetOrders` z atrybutem `[HttpGet]` w `OrdersController.cs`, która zwraca listę zamówień z ich statusem.
    - Dodano metodę `PlaceOrder` z atrybutem `[HttpPost]` w `OrdersController.cs`, która obsługuje składanie zamówień.
    - Zmieniono przestrzeń nazw w `OrderWithStatus.cs` na `BlazingPizza.Model`.
    - Zaktualizowano teksty statusów zamówień w `OrderWithStatus.cs` na język polski.
    - Zmieniono elementy `<a>` na `<NavLink>` w plikach `Checkout.razor` i `Index.razor` dla lepszej nawigacji.
    - Dodano nową stronę `MyOrders.razor`, która wyświetla listę zamówień użytkownika z ich statusami.
    - Zaktualizowano przycisk zamówienia w `Checkout.razor` z tekstem "Umieść zamówienie" na "Zamawiam".
    - Zmieniono nawigację po złożeniu zamówienia w `Checkout.razor` na stronę "myorders".
    - Dodano kod do `MyOrders.razor`, który pobiera i wyświetla zamówienia użytkownika.

--- Kontroler/OrdersController.cs
+++ Kontroler/OrdersController.cs
@@ -1,50 +1,51 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using BlazingPizza.Model;
 using BlazingPizza.Dane;
 
-namespace BlazingPizza.Kontroler
+namespace BlazingPizza.Kontroler;
+
+[Route("orders")]
+[ApiController]
+public class OrdersController : Controller
 {
-    [Route("orders")]
-    [ApiController]
-    public class OrdersController : Controller
+    private readonly PizzaStoreContext _db;
+
+    public OrdersController(PizzaStoreContext db)
     {
-        private readonly PizzaStoreContext _db;
+        _db = db;
+    }
 
-        public OrdersController(PizzaStoreContext db)
+    [HttpGet]
+    public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
+    {
+        var orders = await _db.Orders
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .OrderByDescending(o => o.CreatedTime)
+            .ToListAsync();
+
+        return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> PlaceOrder(Order order)
+    {
+        order.CreatedTime = DateTime.Now;
+
+        // Enforce existence of Pizza.SpecialId and Topping.ToppingId
+        // in the database - prevent the submitter from making up
+        // new specials and toppings
+        foreach (var pizza in order.Pizzas)
         {
-            _db = db;
+            pizza.SpecialId = pizza.Special.Id;
+            pizza.Special = null;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
-        {
-            var orders = await _db.Orders
-             .Include(o => o.Pizzas).ThenInclude(p => p.Special)
-             .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
-             .OrderByDescending(o => o.CreatedTime)
-             .ToListAsync();
+        _db.Orders.Attach(order);
+        await _db.SaveChangesAsync();
 
-            return orders.Select(o => OrderWithStatus.FromOrder(o)).ToList();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<int>> PlaceOrder(Order order)
-        {
-            order.CreatedTime = DateTime.Now;
-
-            // Enforce existence of Pizza.SpecialId and Topping.ToppingId
-            // in the database - prevent the submitter from making up
-            // new specials and toppings
-            foreach (var pizza in order.Pizzas)
-            {
-                pizza.SpecialId = pizza.Special.Id;
-                pizza.Special = null;
-            }
-
-            _db.Orders.Attach(order);
-            await _db.SaveChangesAsync();
-
-            return order.OrderId;
-        }
+        return order.OrderId;
     }
 }
+
